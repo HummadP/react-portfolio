fe2bfcb110662746df8d74ffcc4d559bfb89d032
Hummad <Hummadpathan12@gmail.com>
2023-05-21 12:24:43 +0530

    project done

---
+++ src/Components/About.jsx
@@ -0,0 +1,24 @@
+
+const About = () => {
+    return (
+        <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+                <div className="pb-8">
+                    <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+                </div>
+                <p className="text-xl mt-20">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Culpa unde praesentium quos. Voluptate vitae magni blanditiis. 
+                    Esse, velit magnam maiores at tenetur vel nihil atque quo in rerum vitae repellendus? Eveniet unde saepe, assumenda cupiditate est ullam ut, optio quasi nobis quaerat modi illum totam necessitatibus tempore nulla excepturi repellendus?
+                </p>
+
+                <br />
+                <p className="text-xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                   Quas accusantium adipisci minima asperiores. 
+                   Sequi quis iure perspiciatis, doloribus facere repellat deserunt. 
+                   Esse repellat fugiat, maxime quam quidem amet mollitia voluptates magni natus neque obcaecati quibusdam veritatis libero odio nesciunt itaque porro eum consequatur earum! Velit quod ducimus iusto deleniti nemo.</p>
+            </div>
+        </div>
+    );
+}
+export default About;
