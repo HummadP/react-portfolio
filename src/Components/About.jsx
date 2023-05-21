14ebd73a7d1eab309e2ee56f9b8cdd21a91f0dd8
Hummad <Hummadpathan12@gmail.com>
2023-05-21 12:53:34 +0530

    done

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,16 +7,13 @@ const About = () => {
                     <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
                 </div>
                 <p className="text-xl mt-20">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Culpa unde praesentium quos. Voluptate vitae magni blanditiis. 
-                    Esse, velit magnam maiores at tenetur vel nihil atque quo in rerum vitae repellendus? Eveniet unde saepe, assumenda cupiditate est ullam ut, optio quasi nobis quaerat modi illum totam necessitatibus tempore nulla excepturi repellendus?
+                I'm Hummad, a frontend developer passionate about crafting user-friendly and visually appealing websites. With expertise in HTML, CSS, JavaScript and React, I specialize in creating responsive interfaces that enhance the user experience. I strive to stay updated with the latest technologies and best practices to deliver high-quality work. Let's collaborate to bring your web projects to life!
                 </p>
 
                 <br />
-                <p className="text-xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                   Quas accusantium adipisci minima asperiores. 
-                   Sequi quis iure perspiciatis, doloribus facere repellat deserunt. 
-                   Esse repellat fugiat, maxime quam quidem amet mollitia voluptates magni natus neque obcaecati quibusdam veritatis libero odio nesciunt itaque porro eum consequatur earum! Velit quod ducimus iusto deleniti nemo.</p>
+                <p className="text-xl">As a computer science student, I am continually exploring new technologies and frameworks to stay up to date with the latest trends in the industry. I thrive on challenges and enjoy pushing the boundaries of what is possible in web development. I believe in continuous learning and strive to improve my skills through personal projects and collaborations with other developers.
+                Thank you for visiting my portfolio. I am excited to showcase my projects and share my passion for frontend web development. If you have any inquiries or would like to discuss potential collaborations, please feel free to reach out. Let's work together to create outstanding web experiences!
+                </p>
             </div>
         </div>
     );
