8cc897117585b92616024cabf909285078466d71
Hummad <Hummadpathan12@gmail.com>
2023-05-26 10:53:46 +0530

    some minor tweaks

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,12 +1,12 @@
 
 const About = () => {
     return (
-        <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
-            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-                <div className="pb-8">
+        <div name="about" className="pt-60 md:pt-0 w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+            <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
+                <div className="pb-4">
                     <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
                 </div>
-                <p className="text-xl mt-20">
+                <p className="text-xl mt-10">
                 I'm Hummad, a frontend developer passionate about crafting user-friendly and visually appealing websites. With expertise in HTML, CSS, JavaScript and React, I specialize in creating responsive interfaces that enhance the user experience. I strive to stay updated with the latest technologies and best practices to deliver high-quality work. Let's collaborate to bring your web projects to life!
                 </p>
 
